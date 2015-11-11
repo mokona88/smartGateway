b05c3799a6982fbd29fa2338ff52567a96000bc3
Thong Nguyen <nhthong2007@gmail.com>
2015-11-11 05:19:13 -0800

    Initialize Project

---
+++ src/main/java/sjsu/cmpe/class273/gateway/SmartGatewayApplication.java
@@ -0,0 +1,12 @@
+package sjsu.cmpe.class273.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SmartGatewayApplication.class, args);
+    }
+}
